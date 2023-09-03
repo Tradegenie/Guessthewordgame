0d9c90b437cc1111883ac7309f8c648018222437
Israel Onyeogaziri <tradegenie@Israels-MacBook-Pro.local>
2023-09-03 20:22:41 +0100

    firstcommit

---
+++ js/word.js
@@ -0,0 +1,59 @@
+const wordList = [
+    {
+        word: "python",
+        hint: "programming language"
+    },
+    {
+        word: "guitar",
+        hint: "a musical instrument"
+    },
+    {
+        word: "aim",
+        hint: "a purpose or intention"
+    },
+    {
+        word: "venus",
+        hint: "planet of our solar system"
+    },
+    {
+        word: "gold",
+        hint: "a yellow precious metal"
+    },
+    {
+        word: "golang",
+        hint: "programming language"
+    },
+    {
+        word: "bitcoin",
+        hint: "a cryptocurrency"
+    },
+    {
+        word: "javascript",
+        hint: "programming language"
+    },
+    {
+        word: "intermiami",
+        hint: "a football club"
+    },
+    {
+        word: "ethereum",
+        hint: "created by vitalik"
+    },
+    {
+        word: "gtbank",
+        hint: "most poular jingle"
+    },
+    {
+        word: "lotusbank",
+        hint: "purple logo"
+    },
+    {
+        word: "whistleblower",
+        hint: "reports illegal activity"
+    },
+    {
+        word: "enaira",
+        hint: "digital currency in nigeria"
+    }
+
+]
